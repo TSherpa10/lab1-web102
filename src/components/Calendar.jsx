e51f314ac16286b7f3d05100ddc12fb9b74fb141
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2023-09-16 14:53:34 -0400

    added files

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import Event from './Event'
+
+const Calendar = () => {
+    return (
+        <div className="Calendar">
+            <table>
+            <thead>
+                <tr>
+                <th></th>
+                <th>Sunday</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="time">8 am</td>
+                    <Event event='Fancy Dinner 🎩' color ='green'/>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <Event event='Yolk 🍳' color ='green'/>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">9 am</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <Event event='Subway 🚊' color ='pink'/>
+                    <td></td>
+                    <td></td>
+                    <Event event='The Bean 🫘' color ='blue'/>
+                </tr>
+                <tr>
+                    <td className="time">10 am</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <Event event='The Bean 🫘' color ='blue' location='New York'/>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">11 am</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <Event event='Art Museum' color ='pink'/>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">12 pm</td>
+                    <td></td>
+                    <td></td>
+                    <Event event='Cafe' color ='blue'/>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">1 pm</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <Event event='Subway 🚊' color ='pink'/>
+                </tr>
+                <tr>
+                    <td className="time">2 pm</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">3 pm</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">4 pm</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td className="time">5 pm</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Calendar;
